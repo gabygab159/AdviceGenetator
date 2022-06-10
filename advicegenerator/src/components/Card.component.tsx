7ab7a2947e7737d38aa76338fe1869dacb105019
gaby <gabriel.jchen@gmail.com>
2022-06-09 23:20:05 -0400

    basic styling done

--- advicegenerator/src/components/Card.component.tsx
+++ advicegenerator/src/components/Card.component.tsx
@@ -1,3 +1,15 @@
+import desktopDivider from "../images/pattern-divider-desktop.svg";
+import "./Card.css";
+
 export function Card() {
-  return <div>Card</div>;
+  return (
+    <>
+      <div className="card">
+        <h3 className="card-header">Advice</h3>
+        <p className="card-text">This is where the advice goes</p>
+        <img src={desktopDivider}></img>
+        <button className="card-button"></button>
+      </div>
+    </>
+  );
 }
