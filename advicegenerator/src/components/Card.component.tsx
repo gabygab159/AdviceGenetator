d60be28eaf2c039efca69f17f1f21a0183985fb7
Gabriel Chen <gabriel.jchen@gmail.com>
2022-06-10 16:37:03 -0400

    create get request and render advice

--- advicegenerator/src/components/Card.component.tsx
+++ advicegenerator/src/components/Card.component.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import desktopDivider from "../images/pattern-divider-desktop.svg";
 import "./Card.css";
 
+interface IAdvice {
+  advice: string;
+  id: number;
+}
+
 export function Card() {
+  const [advice, setAdvice] = useState<IAdvice>()
+
+  const handleClick = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then(response => response.json())
+      .then(data => { setAdvice(data.slip) })
+      .then(() => console.log(advice))
+  }
+
   return (
     <>
       <div className="card">
-        <h3 className="card-header">Advice</h3>
-        <p className="card-text">This is where the advice goes</p>
+        <h3 className="card-header">Advice #{advice?.id}</h3>
+        <p className="card-text">{advice?.advice}</p>
         <img src={desktopDivider}></img>
-        <button className="card-button"></button>
+        <button className="card-button" onClick={handleClick}></button>
       </div>
     </>
   );
